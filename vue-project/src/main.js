c39624f2c5f52a66c8558c817af184694883057f
ulpaav <zulfameydita@students.amikom.ac.id>
2025-07-06 13:53:24 +0700

    tambah profil, pendidikan, skill, proyek, kontak

---
+++ vue-project/src/main.js
@@ -0,0 +1,10 @@
+import './assets/main.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import animateOnScroll from './directives/animateOnScroll';
+
+const app = createApp(App)
+app.directive('animate-on-scroll', animateOnScroll);
+app.use(router)
+app.mount('#app')
