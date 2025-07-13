400fef3c7d37b62a5bdac31610869ea672091e5a
ulpaav <zulfameydita@students.amikom.ac.id>
2025-07-13 21:06:27 +0700

    Fix warning

--- backend/data.js
+++ backend/data.js
@@ -29,28 +29,28 @@ export const skills = [
 export const projects = [
     {
         title: 'Website Toko Online',
-        image: 'https://via.placeholder.com/500x300?text=Proyek+1',
+        image: '/images/website_tooko_online.webp',
         description: 'Platform e-commerce dengan fitur keranjang belanja.',
         tech: ['Vue.js', 'Express.js', 'PostgreSQL'],
         link: '#'
     },
     {
         title: 'Aplikasi Manajemen Tugas',
-        image: 'https://via.placeholder.com/500x300?text=Proyek+2',
+        image: '/images/project-management.webp',
         description: 'Aplikasi untuk melacak progres tugas harian.',
         tech: ['React', 'Firebase'],
         link: '#'
     },
     {
         title: 'Sistem Booking Online',
-        image: 'https://via.placeholder.com/500x300?text=Proyek+3',
+        image: '/images/booking-online.webp',
         description: 'Aplikasi booking untuk reservasi layanan.',
         tech: ['Next.js', 'MongoDB'],
         link: '#'
     },
     {
         title: 'Dashboard Admin',
-        image: 'https://via.placeholder.com/500x300?text=Proyek+4',
+        image: '/images/dashboard.webp',
         description: 'Dashboard admin dengan data real-time.',
         tech: ['Laravel', 'MySQL'],
         link: '#'
